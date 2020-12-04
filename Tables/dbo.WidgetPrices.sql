2e1d5bfa8ef4e2f100d0d5e64b93a5ca9402a492
Rene <rene.garza@red-gate.com>
2020-12-04 13:53:52 -0600

    Initial Commit

---
+++ Tables/dbo.WidgetPrices.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[WidgetPrices]
+(
+[RecordID] [int] NOT NULL IDENTITY(1, 1),
+[WidgetID] [int] NULL,
+[Price] [money] NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[WidgetPrices] ADD CONSTRAINT [PK_WidgetPrices] PRIMARY KEY NONCLUSTERED  ([RecordID]) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [IX_WidgetPrices] ON [dbo].[WidgetPrices] ([WidgetID]) ON [PRIMARY]
+GO
